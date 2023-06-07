4359d05d6f7540af5a446904a0af4ff7bc06b900
User <jeisonj2019@gmail.com>
2023-06-07 05:07:14 +0500

    Обновлён компонент 'register'. Разметка перенесена из другого приложения

--- client/src/app/account/register/register.component.ts
+++ client/src/app/account/register/register.component.ts
@@ -1,4 +1,11 @@
 import { Component } from '@angular/core';
+import { HttpClient, HttpResponse } from '@angular/common/http';
+import { EMPTY, catchError } from 'rxjs';
+import { ActivatedRoute, Router } from '@angular/router';
+import { FormControl } from '@angular/forms';
+import { ErrorHandlerService } from 'src/app/services/errors/error-handler.service';
+import { Consts } from 'src/app/consts';
+const baseUrl = { location };
 
 @Component({
   selector: 'app-register',
@@ -6,5 +13,34 @@ import { Component } from '@angular/core';
   styleUrls: ['./register.component.scss']
 })
 export class RegisterComponent {
-
+  c: Consts = new Consts();
+  constructor(private http: HttpClient,
+    private route: ActivatedRoute,
+    private router: Router,
+    private errorHandler: ErrorHandlerService) {
+    errorHandler.pubmEvent.subscribe(
+      (pubm) => {
+        this.pubm = pubm;
+      }
+    )
+   }
+  pubm: string | undefined;
+  showWarning: boolean = false;
+  waiting: boolean = false;
+  register(f: FormControl): void {
+    let url = this.c.URL_REGISTER;
+    let postObservable = this.http.post(url, f);
+    this.waiting = true;
+    postObservable.subscribe({
+      next:(data) => {
+        this.router.navigateByUrl('/account/register/confirmation');
+        this.waiting = false;
+      },
+      error: (error) => {
+        this.showWarning = true;
+        this.errorHandler.handleError(error);
+        this.waiting = false;
+      }
+  });
+  }
 }
