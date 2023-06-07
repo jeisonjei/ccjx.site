2f00b162b30a157609e6cb7a97fbcd3608571ea2
User <jeisonj2019@gmail.com>
2023-06-07 05:06:23 +0500

    Обновлены константы

--- client/src/app/consts.ts
+++ client/src/app/consts.ts
@@ -1,15 +1,13 @@
-import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 
 export class Consts {
   baseUrl: string = `http://${location.hostname}:5000/api`;
   constructor() {}
-  public displayNameMap = new Map([
-    [Breakpoints.XSmall, 'XSmall'],
-    [Breakpoints.Small, 'Small'],
-    [Breakpoints.Medium, 'Medium'],
-    [Breakpoints.Large, 'Large'],
-    [Breakpoints.XLarge, 'XLarge'],
-  ]);
+  public DEFAULT_ERROR_MESSAGE = 'Извините, возникла ошибка';
   public URL_TOKEN = `${this.baseUrl}/token/`;
   public URL_TOKEN_REFRESH = `${this.baseUrl}/token/refresh/`;
+  public URL_REGISTER = `${this.baseUrl}/accounts/register/`;
+}
+export type ServerError = {
+  public: string,
+  private:string
 }
