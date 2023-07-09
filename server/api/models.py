d5395e2c50b871c581aaf095f7cf1eb3ce3ce597
jeisonjei <jeisonj2019@gmail.com>
2023-07-09 06:36:51 +0500

    Поправлена работа с административным сайтом

--- server/api/models.py
+++ server/api/models.py
@@ -19,13 +19,12 @@ class UserManager(BaseUserManager):
         return self._create_user(email,password,**extra_fields)
 
     def create_superuser(self,email,password,**extra_fields):
-        extra_fields.setdefault('is_staff',True)
-        extra_fields.setdefault('is_superuser',True)
-
-        if extra_fields.get('is_staff') is not True:
-            raise ValueError('Поле "is_staff" суперпользователя должно быть True')
-        if extra_fields.get('is_superuser') is not True:
-            raise ValueError('Поле "is_superuser" супервользователя должно быть True')
+        user = self.create_user(email, password=password)
+        user.is_admin = True
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using = self._db)
+        return user
         
 class User(AbstractUser):
     USERNAME_FIELD='email'
