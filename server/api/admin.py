012489c5c8d2c7204d5637746d0a5780e388c395
jeisonjei <jeisonj2019@gmail.com>
2023-07-10 07:38:35 +0500

    Добавлены модели к административному сайту

--- server/api/admin.py
+++ server/api/admin.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
-from .models import User
+from .models import Answer, Comment, Topic, User
 
 class UserAdmin(admin.ModelAdmin):
     pass
 
 admin.site.register(User,UserAdmin)
+admin.site.register(Topic)
+admin.site.register(Answer)
+admin.site.register(Comment)
