88bcc37317efb4c3438b2213aeca448da78a883d
jeisonjei <jeisonj2019@gmail.com>
2023-07-14 20:03:28 +0500

    Появилась возможность добавить форматированный ответ

--- client/src/app/components/answer/answer.component.ts
+++ client/src/app/components/answer/answer.component.ts
@@ -3,27 +3,34 @@ import { Answer } from '../../consts';
 import { Comment } from '@angular/compiler';
 import { AuthenticationService } from 'src/app/services/authentication/authentication.service';
 import { DialogService } from 'src/app/services/dialog.service';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-answer',
   templateUrl: './answer.component.html',
-  styleUrls: ['./answer.component.scss']
+  styleUrls: ['./answer.component.scss'],
 })
-export class AnswerComponent implements OnInit{
+export class AnswerComponent implements OnInit {
   @Input()
-  answer?: any
+  answer?: any;
   @Output()
-  commentCreated: EventEmitter<boolean> = new EventEmitter;
+  commentCreated: EventEmitter<boolean> = new EventEmitter();
   newCommentDisplay?: boolean;
   comments: Comment[] = [];
-  constructor(private auth:AuthenticationService,private dials:DialogService) { }
+  constructor(
+    private auth: AuthenticationService,
+    private dials: DialogService,
+    public sanitizer: DomSanitizer
+  ) {}
   ngOnInit(): void {
     this.comments = this.answer.comments;
-
   }
   comment() {
     if (!this.auth.cu?.isLoggedIn) {
-      this.dials.showMessDial('Информация','Чтобы добавить ответ или оставить комментарий, нужно зарегистрироваться')
+      this.dials.showMessDial(
+        'Информация',
+        'Чтобы добавить ответ или оставить комментарий, нужно зарегистрироваться'
+      );
     } else {
       this.newCommentDisplay = true;
     }
--- client/src/app/components/new-answer/new-answer.component.ts
+++ client/src/app/components/new-answer/new-answer.component.ts
@@ -1,6 +1,7 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { FormControl, FormGroup, NgForm } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
+import Quill from 'quill';
 import { Answer } from 'src/app/consts';
 import { AnswerService } from 'src/app/services/answer.service';
 import { AuthenticationService } from 'src/app/services/authentication/authentication.service';
@@ -15,13 +16,40 @@ export class NewAnswerComponent {
   public display: EventEmitter<boolean> = new EventEmitter();
   @Output()
   public created: EventEmitter<boolean> = new EventEmitter();
+  modules = {};
+  content='';
   constructor(private arouter: ActivatedRoute,private auth:AuthenticationService,private answer:AnswerService) {
-
+    this.modules = {
+      formula: true,
+      syntax: true,
+      toolbar: [
+        ['bold', 'italic', 'underline', 'strike'],        // toggled buttons
+        ['blockquote', 'code-block'],
+    
+        [{ 'header': 1 }, { 'header': 2 }],               // custom button values
+        [{ 'list': 'ordered'}, { 'list': 'bullet' }],
+        [{ 'script': 'sub'}, { 'script': 'super' }],      // superscript/subscript
+        [{ 'indent': '-1'}, { 'indent': '+1' }],          // outdent/indent
+        [{ 'direction': 'rtl' }],                         // text direction
+    
+        [{ 'size': ['small', false, 'large', 'huge'] }],  // custom dropdown
+        [{ 'header': [1, 2, 3, 4, 5, 6, false] }],
+    
+        [{ 'color': [] }, { 'background': [] }],          // dropdown with defaults from theme
+        [{ 'font': [] }],
+        [{ 'align': [] }],
+    
+        ['clean'],                                         // remove formatting button
+    
+        ['link', 'image', 'video'],                         // link and image, video
+        ['formula']
+      ]
+    };
    }
   addAnswer(form: NgForm) {
     const topicId = this.arouter.snapshot.paramMap.get('topicId');
     const userId = this.auth.cu?.id??'';
-    const text = form.value.text;
+    const text = this.content;
     const answer :Answer= {
       topic: topicId??'',
       user: userId,
@@ -35,4 +63,5 @@ export class NewAnswerComponent {
   hideForm() {
     this.display.emit(false);
   }
+  addBindingCreated(quill: Quill){}
 }
