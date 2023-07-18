8375a3d214ddc32bb89ccf04b23b596e22b111eb
jeisonjei <jeisonj2019@gmail.com>
2023-07-18 22:39:28 +0500

    Изменён размер шрифта по умолчанию

--- client/src/app/shared/components/common-editor-rich/common-editor-rich.component.ts
+++ client/src/app/shared/components/common-editor-rich/common-editor-rich.component.ts
@@ -25,25 +25,20 @@ export class CommonEditorRichComponent {
       imageResize:true,
       toolbar: [
         ['bold', 'italic', 'underline', 'strike'],        // toggled buttons
-        ['blockquote', 'code-block','code'],
-    
-        [{ 'header': 1 }, { 'header': 2 }],               // custom button values
-        [{ 'list': 'ordered'}, { 'list': 'bullet' }],
-        [{ 'script': 'sub'}, { 'script': 'super' }],      // superscript/subscript
-        [{ 'indent': '-1'}, { 'indent': '+1' }],          // outdent/indent
-        [{ 'direction': 'rtl' }],                         // text direction
-    
         [{ 'size': ['small', false, 'large', 'huge'] }],  // custom dropdown
         [{ 'header': [1, 2, 3, 4, 5, 6, false] }],
-    
-        [{ 'color': [] }, { 'background': [] }],          // dropdown with defaults from theme
         [{ 'font': [] }],
         [{ 'align': [] }],
+        [{ 'script': 'sub'}, { 'script': 'super' }],      // superscript/subscript
+        [{ 'list': 'ordered'}, { 'list': 'bullet' }],
     
-        ['clean'],                                         // remove formatting button
     
+        [{ 'color': [] }, { 'background': [] }],          // dropdown with defaults from theme
+    
+        
         ['link', 'image', 'video'],                         // link and image, video
-        ['formula']
+        ['blockquote','code','code-block','formula'],
+        ['clean'],                                         // remove formatting button
       ]
     };
   }
