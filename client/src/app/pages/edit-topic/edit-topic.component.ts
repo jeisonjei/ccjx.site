c10e66e47982c0821e849d5113658b502b7e0bee
jeisonjei <jeisonj2019@gmail.com>
2023-07-29 23:24:04 +0500

    Тэги тем можно редактировать

--- client/src/app/pages/edit-topic/edit-topic.component.ts
+++ client/src/app/pages/edit-topic/edit-topic.component.ts
@@ -1,12 +1,14 @@
 import { Component, OnInit } from '@angular/core';
-import { NgForm } from '@angular/forms';
+import { FormControl, NgForm } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
 import { TopicService } from '@app/services/question.service';
 import { UrlsService } from '@app/services/urls.service';
-import { Topic } from "@app/consts";
+import { Tag, Topic } from "@app/consts";
 import * as QuillNamespace from 'quill';
 let Quill: any = QuillNamespace;
 import ImageResize from 'quill-image-resize-module';
+import { TagService } from '@app/services/tag.service';
+import { Observable, map, startWith } from 'rxjs';
 Quill.register('modules/imageResize', ImageResize);
 
 @Component({
@@ -21,13 +23,35 @@ export class EditTopicComponent implements OnInit {
   content = '';
   isArticle: boolean=false;
   isPrivate: boolean=false;
+  myControl: FormControl=new FormControl('');
+  tags: any[]=[];
+  tagsFiltered?: Observable<any[]>;
+  tagsAdded: any[]=[];
+  tagSelected:any =null;
   constructor(
     private activatedRoute: ActivatedRoute,
     private topicService: TopicService,
     private router: Router,
-    private urls: UrlsService
+    private urls: UrlsService,
+    private tagService: TagService
   ) {}
   ngOnInit(): void {
+    this.getTopic();
+    this.tagList();
+    this.tagsFiltered = this.myControl.valueChanges.pipe(
+      startWith(''),
+      map(v => this._filter(v || ''))
+    );
+  }
+  private _filter(v: any): any {
+    if (typeof (v) == 'object') {
+      // при выборе элемента сюда передаётся объект тэг. В этом случае возврат
+      return;
+    }
+    const filterValue = v.toLowerCase(); 
+    return this.tags.filter(v=>v.name.toLowerCase().includes(filterValue));
+  }
+  getTopic() {
     this.userId =
       this.activatedRoute.snapshot.paramMap.get('userId') ?? 'error';
     this.topicId =
@@ -37,8 +61,8 @@ export class EditTopicComponent implements OnInit {
       this.content = v.text;
       this.isArticle = v.is_article;
       this.isPrivate = v.is_private;
+      this.tagsAdded = v.tags;
     });
-    
   }
   edit(content: string) {
     const topic: Topic = {
@@ -69,5 +93,52 @@ export class EditTopicComponent implements OnInit {
   onIsPrivateChange(checked: boolean) {
     this.isPrivate = checked;
   }
+  tagList() {
+    this.tagService.list().subscribe((v:any) => {
+      this.tags = v;
+    })    
+  }
+  tagCreate(tagName: string) {
+    if (!this.tags.map(v=>v.name).includes(tagName)) {
+      // создать новый тэг
+      const tag:Tag = {
+        name: tagName,
+        topics: [this.topicId]
+      }
+      this.tagService.create(tag).subscribe(v => {
+        this.tagsAdded.push(v);
+      });
+    }
+    else {
+      // получить id существующего тэга
+      let tag = this.tags.filter(v => v.name === tagName)[0];
+      tag.topics.push(Number.parseInt(this.topicId));
+      this.tagsAdded.push(tag);
+      this.tagService.updateMany(this.tagsAdded)
+    }
+    this.myControl.reset();
+  }
+  tagAddOnEnter(event: Event) {
+    const keyEvent = event as KeyboardEvent;
+    if (keyEvent.key != "Enter") return;
+    const inputField = event.target as HTMLInputElement;
+    const v = inputField.value;
+    this.tagCreate(v);
+  }
+  tagDelete(tag: Tag) {
+    const index = tag.topics.findIndex(v=>v==this.topicId);
+    tag.topics.splice(index,1);
+    console.log(`🔥 tag: ${JSON.stringify(tag)}`);
+    this.tagService.deleteFromTopic(tag?.id??'error',{topics:tag.topics}).subscribe(v => {
+      this.getTopic();
+    })
+  }
+  tagSelectionChange(event: Event) {
+    const inputField = event.target as HTMLInputElement;
+    const v = inputField.value;
+  }
+  displayFn(tag: any) {
+    return tag && tag.name ? tag.name : '';
+  } 
 
 }
