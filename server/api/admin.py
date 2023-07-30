6143d110005bbd6d4966d1595a9f7506828c3db9
jeisonjei <jeisonj2019@gmail.com>
2023-07-30 21:50:37 +0500

    Зарегестрирована модель Tag

--- server/api/admin.py
+++ server/api/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Answer, Comment, Topic, User, Vote
+from .models import Answer, Comment, Tag, Topic, User, Vote
 
 class UserAdmin(admin.ModelAdmin):
     pass
@@ -9,3 +9,4 @@ admin.site.register(Topic)
 admin.site.register(Answer)
 admin.site.register(Comment)
 admin.site.register(Vote)
+admin.site.register(Tag)
