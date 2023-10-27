341da3775a5510618977c90a3d53f0c4ad08313c
jeisonjei <jeisonj2019@gmail.com>
2023-10-27 12:21:15 +0500

    Добавлена кнопка "Создать запись" на главной странице, увеличена область документа на страницах "Topic"

--- client/src/app/pages/popular-and-not/popular-and-not.component.ts
+++ client/src/app/pages/popular-and-not/popular-and-not.component.ts
@@ -1,7 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { Route, Router } from '@angular/router';
 import { Tag, Topic } from '@app/consts';
+import { AuthenticationService } from '@app/services/authentication/authentication.service';
+import { DialogService } from '@app/services/dialog.service';
 import { TopicService } from '@app/services/question.service';
 import { TagService } from '@app/services/tag.service';
+import { ValidatorService } from '@app/services/validator.service';
 
 @Component({
   selector: 'app-popular-and-not',
@@ -11,7 +15,7 @@ import { TagService } from '@app/services/tag.service';
 export class PopularAndNotComponent implements OnInit{
   tags: Tag[]=[];
   recentQuestions:Topic[]=[];
-constructor(private tagService:TagService, private topicService:TopicService){}
+  constructor(private tagService: TagService, private topicService: TopicService, private auth:AuthenticationService,private dials:DialogService,private validator:ValidatorService,private tops:TopicService,private router:Router){}
   ngOnInit(): void {
     this.tagService.list().subscribe((v: any) => {
       this.tags = v;
@@ -20,4 +24,25 @@ constructor(private tagService:TagService, private topicService:TopicService){}
       this.recentQuestions = v;
     })
   }
+  onQuestion(value: string) {
+    if (!this.auth.userValue?.isLoggedIn) {
+      this.dials.showMessDial('Информация','Чтобы создавать вопросы, нужно зарегистрироваться');
+    }
+    else{
+      if (!this.validator.text(value)) return;
+      const userId = this.auth.userValue?.id;
+      const question: Topic = {
+        user: userId,
+        title: value
+      };
+      const self = this;
+      this.tops.create(question).subscribe({
+        next(value: { user: { id: any; }; id: any; }) {
+          const url = `users/${value.user.id}/new-topic/${value.id}`;
+          self.router.navigateByUrl(url);
+        },
+      });
+    }
+  }
+
 }
