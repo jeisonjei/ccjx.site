226d091961f217cbe2dbd3130298c9becc64485c
jeisonjei <jeisonj2019@gmail.com>
2023-10-29 15:45:52 +0500

    settings.py updated

--- server/server/settings.py
+++ server/server/settings.py
@@ -14,6 +14,8 @@ from pathlib import Path
 import os
 from nameof import nameof
 from datetime import timedelta
+import mimetypes
+mimetypes.add_type("text/css","css",True)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
