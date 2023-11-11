9a837033b9ce016c08355d90e78137d651b26bf2
jeisonjei <jeisonj2019@gmail.com>
2023-11-11 21:59:58 +0500

    Перестали добавляться вопросы, ошибка исправлена

--- client/src/app/components/new-answer/new-answer.component.ts
+++ client/src/app/components/new-answer/new-answer.component.ts
@@ -20,36 +20,12 @@ export class NewAnswerComponent {
   modules = {};
   content = '';
   constructor(private arouter: ActivatedRoute,private auth:AuthenticationService,private ans:AnswerService) {
-    this.modules = {
-      formula: true,
-      syntax: true,
-      imageResize:true,
-      toolbar: [
-        ['bold', 'italic', 'underline', 'strike'],        // toggled buttons
-        ['blockquote', 'code-block'],
     
-        [{ 'header': 1 }, { 'header': 2 }],               // custom button values
-        [{ 'list': 'ordered'}, { 'list': 'bullet' }],
-        [{ 'script': 'sub'}, { 'script': 'super' }],      // superscript/subscript
-        [{ 'indent': '-1'}, { 'indent': '+1' }],          // outdent/indent
-        [{ 'direction': 'rtl' }],                         // text direction
-    
-        [{ 'size': ['small', false, 'large', 'huge'] }],  // custom dropdown
-        [{ 'header': [1, 2, 3, 4, 5, 6, false] }],
-    
-        [{ 'color': [] }, { 'background': [] }],          // dropdown with defaults from theme
-        [{ 'font': [] }],
-        [{ 'align': [] }],
-    
-        ['clean'],                                         // remove formatting button
-    
-        ['link', 'image', 'video'],                         // link and image, video
-        ['formula']
-      ]
-    };
    }
   addAnswer(content: string) {
-    const topicId = this.arouter.snapshot.paramMap.get('topicId');
+    
+    const param = this.arouter.snapshot.paramMap.get('topicSlug');
+    const topicId = param?.split('-')[0];
     const userId = this.auth.userValue?.id??'';
     const text = content;
     const answer :Answer= {
@@ -65,5 +41,4 @@ export class NewAnswerComponent {
   hideForm() {
     this.display.emit(false);
   }
-  addBindingCreated(quill: any){}
 }
