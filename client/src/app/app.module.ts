2e0eaa3982b1a0de3347b271858648f6dc0585d7
jeisonjei <jeisonj2019@gmail.com>
2024-03-17 21:49:19 +0500

    @angular/ssr added

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
 import { HttpClientModule } from "@angular/common/http";
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -135,6 +135,8 @@ import { MyTagsComponent } from './pages/my-tags/my-tags.component';
     MatRadioModule
   ],
   providers: [
+  
+    provideClientHydration()
   ],
   bootstrap: [AppComponent]
 })
